e5655ab270ca3121dbf2abf866060e4b036e9085
rulyadhika <rulihanif@gmail.com>
2024-03-20 15:17:42 +0700

    bugfix: validation errors can now be handled

--- middleware/error_handler_middleware.go
+++ middleware/error_handler_middleware.go
@@ -1,9 +1,11 @@
 package middleware
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 	"github.com/rulyadhika/my_gram_final_asgmt/model/dto"
 	"github.com/rulyadhika/my_gram_final_asgmt/pkg/errs"
 )
@@ -20,6 +22,8 @@ func ErrorHandlerMiddleware() gin.HandlerFunc {
 				handleError(ctx, http.StatusUnprocessableEntity, err)
 			case *errs.BadRequestError:
 				handleError(ctx, http.StatusBadRequest, err)
+			case validator.ValidationErrors:
+				handleValidationError(ctx, err.Err.(validator.ValidationErrors))
 			case *errs.NotFoundError:
 				handleError(ctx, http.StatusNotFound, err)
 			default:
@@ -30,7 +34,7 @@ func ErrorHandlerMiddleware() gin.HandlerFunc {
 }
 
 func handleError(ctx *gin.Context, httpStatusCode int, err error) {
-	webResponse := &dto.CommonDto{
+	webResponse := &dto.WebResponse{
 		Status:  http.StatusText(httpStatusCode),
 		Code:    httpStatusCode,
 		Message: err.Error(),
@@ -38,4 +42,25 @@ func handleError(ctx *gin.Context, httpStatusCode int, err error) {
 	}
 
 	ctx.AbortWithStatusJSON(httpStatusCode, webResponse)
+}
+
+func handleValidationError(ctx *gin.Context, errs validator.ValidationErrors) {
+	var errors string
+
+	for index, err := range errs {
+		errors += fmt.Sprintf("Field validation for '%s' failed on the '%s' tag", err.Field(), err.Tag())
+
+		if index != len(errs)-1 {
+			errors += ","
+		}
+	}
+
+	webResponse := &dto.WebResponse{
+		Status:  http.StatusText(http.StatusBadRequest),
+		Code:    http.StatusBadRequest,
+		Message: errors,
+		Data:    nil,
+	}
+
+	ctx.AbortWithStatusJSON(http.StatusBadRequest, webResponse)
 }
