9c705f453060901f5be5dd2542b19b86d692c0ba
rulyadhika <rulihanif@gmail.com>
2024-03-20 12:35:51 +0700

    bugfix: cannot connect to db

--- app/database.go
+++ app/database.go
@@ -4,12 +4,14 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+
+	_ "github.com/lib/pq"
 )
 
 func InitDB() *sql.DB {
 	appConfig := GetAppConfig()
 
-	dsn := fmt.Sprintf("host:%s port:%s database:%s username:%s password:%s", appConfig.DB_HOST, appConfig.DB_PORT, appConfig.DB_DATABASE, appConfig.DB_USERNAME, appConfig.DB_PASSWORD)
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", appConfig.DB_HOST, appConfig.DB_PORT, appConfig.DB_USERNAME, appConfig.DB_PASSWORD, appConfig.DB_DATABASE)
 
 	db, err := sql.Open(appConfig.DB_DRIVER, dsn)
 
